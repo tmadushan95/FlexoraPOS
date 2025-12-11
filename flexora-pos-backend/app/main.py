60e14fa9332cc7882e429c673d9d1bc376944f1f
Tharindu Madushan <30996054+tmadushan95@users.noreply.github.com>
2025-12-11 10:38:44 +0530

    delete file

--- flexora-pos-backend/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.login_router import router as user_router
-
-app = FastAPI(title="Clean Architecture FastAPI App")
-
-app.include_router(user_router, prefix="/api/v1/users", tags=["users"])
